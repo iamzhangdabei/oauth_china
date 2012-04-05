cab270b23ee92d99749c1a3d509cc2fceaaa01c4
zhangdabei <keepyouliking@gmail.com>
2012-04-05 13:50:23 +0800

    Update lib/oauth_china/strategies/sina.rb

--- lib/oauth_china/strategies/sina.rb
+++ lib/oauth_china/strategies/sina.rb
@@ -23,7 +23,11 @@ module OauthChina
     def destroy
       #TODO
     end
-
+    
+    def get_my_messages
+      self.get("http://api.t.sina.com.cn/account/verify_credentials.json").body
+    end
+    
     def add_status(content, options = {})
       options.merge!(:status => content)
       self.post("http://api.t.sina.com.cn/statuses/update.json", options)
